7e472bcb2287c22537599e0332dff1d3f6c9aa51
Soner Sayakci <s.sayakci@gmail.com>
2025-02-06 14:38:46 +0100

    feat: add custom StyleLint rule to enforce SCSS import conventions

--- tools/stylelint/administration.config.mjs
+++ tools/stylelint/administration.config.mjs
@@ -4,12 +4,14 @@ export default {
         "stylelint-config-standard"
     ],
     plugins: [
-        "stylelint-scss"
+        "stylelint-scss",
+        "./rules/administration/wrong-scss-import.js"
     ],
     rules: {
         "selector-class-pattern": null,
         "import-notation": null,
         "declaration-property-value-no-unknown": null,
         "at-rule-no-unknown": null,
+        "shopware-administration/no-scss-extension-import": true
     }
 };
