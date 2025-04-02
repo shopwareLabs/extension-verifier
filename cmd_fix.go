c490bcc6bff5643acd205e9e7f6418f1636b3593
Shyim <github@shyim.de>
2025-04-02 18:27:50 +0200

    Fix cmd_fix to check if the provided path is a project
    Add a check in `cmd_fix.go` to determine if the provided path is a project.
    
    * Use `tool.IsProject` to check if the provided path is a project.
    * If the path is a project, retrieve the tool configuration using `tool.GetConfigFromProject`.
    * If the path is not a project, retrieve the extension by folder and convert it to a tool configuration using `tool.ConvertExtensionToToolConfig`.
    * Update the logic to be consistent with `cmd_check.go` for determining if the provided path is a project.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/shopwareLabs/extension-verifier?shareId=XXXX-XXXX-XXXX-XXXX).

--- cmd_fix.go
+++ cmd_fix.go
@@ -25,16 +25,24 @@ var (
 				}
 			}
 
-			ext, err := extension.GetExtensionByFolder(args[0])
+			var toolCfg *tool.ToolConfig
+			var err error
 
-			if err != nil {
-				return err
-			}
+			if tool.IsProject(args[0]) {
+				toolCfg, err = tool.GetConfigFromProject(args[0])
+				if err != nil {
+					return err
+				}
+			} else {
+				ext, err := extension.GetExtensionByFolder(args[0])
+				if err != nil {
+					return err
+				}
 
-			toolCfg, err := tool.ConvertExtensionToToolConfig(ext)
-
-			if err != nil {
-				return err
+				toolCfg, err = tool.ConvertExtensionToToolConfig(ext)
+				if err != nil {
+					return err
+				}
 			}
 
 			var gr errgroup.Group
